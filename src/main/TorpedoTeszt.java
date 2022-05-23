d790aed1c30fb2fff00ed117109e6877a02905d9
PIKSSL <purpledog120@gmail.com>
2022-05-23 12:36:48 +0200

    tesztLoves()

--- src/main/TorpedoTeszt.java
+++ src/main/TorpedoTeszt.java
@@ -2,15 +2,17 @@
 package main;
 
 public class TorpedoTeszt {
-    
+    private Hajo hajo = new Hajo();
     public static void main(String[] args) {
         System.out.println("TESZT");
-        
+        //assert 3==5:"Nem jó a találat ellenőrzése";
+        new TorpedoTeszt().tesztLoves(4);
     }
     
     public String tesztLoves(int poz){
-        
-        return "";
+        String t = hajo.talalat(poz);
+        assert t.equals("talált"):"nem jó a találta ellenőrzése";
+        return "jo";
     }
     
     
